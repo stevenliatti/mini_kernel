fffa91eaa98a3e8ffa33be9a709bc9fcc4599f11
steven.liatti <steven.liatti@etu.hesge.ch>
2017-11-08 16:44:52 +0100

    IDT (part 1) done I think
    On branch master
    
    Changes to be committed:
    	modified:   kernel/idt.c
    	modified:   kernel/idt.h
    	modified:   kernel/idt_asm.s
    	modified:   kernel/timer.c

--- kernel/idt.c
+++ kernel/idt.c
@@ -1,3 +1,17 @@
+#include "base.h"
+#include "idt.h"
+#include "screen.h"
+#include "pic.h"
+#include "x86.h"
+
+#define IDT_SIZE 256
+
+// Declaration of IDT
+static idt_entry_t idt[IDT_SIZE];
+
+// Pointer on the IDT
+static idt_ptr_t idt_ptr;
+
 // CPU context used when saving/restoring context from an interrupt
 typedef struct regs_st {
 	uint32_t gs, fs, es, ds;
@@ -26,10 +40,185 @@ static idt_entry_t idt_build_entry(uint16_t selector, uint32_t offset, uint8_t t
 
 // Exception handler
 void exception_handler(regs_t *regs) {
+	clr_scr();
+	set_theme(BLACK, RED);
+	switch (regs->number) {
+		case 0:
+			printf("Exception 0 - Divide Error");
+			break;
+		case 1:
+			printf("Exception 1 - RESERVED");
+			break;
+		case 2:
+			printf("Exception 2 - NMI Interrupt");
+			break;
+		case 3:
+			printf("Exception 3 - Breakpoint");
+			break;
+		case 4:
+			printf("Exception 4 - Overflow");
+			break;
+		case 5:
+			printf("Exception 5 - BOUND Range Exceeded");
+			break;
+		case 6:
+			printf("Exception 6 - Invalid Opcode (Undefined Opcode)");
+			break;
+		case 7:
+			printf("Exception 7 - Device Not Available (No Math Coprocessor)");
+			break;
+		case 8:
+			printf("Exception 8 - Double Fault");
+			break;
+		case 9:
+			printf("Exception 9 - Coprocessor Segment Overrun (reserved)");
+			break;
+		case 10:
+			printf("Exception 10 - Invalid TSS");
+			break;
+		case 11:
+			printf("Exception 11 - Segment Not Present");
+			break;
+		case 12:
+			printf("Exception 12 - Stack-Segment Fault");
+			break;
+		case 13:
+			printf("Exception 13 - General Protection");
+			break;
+		case 14:
+			printf("Exception 14 - Page Fault");
+			break;
+		case 15:
+			printf("Exception 15 - (Inter reserved. Do not use.)");
+			break;
+		case 16:
+			printf("Exception 16 - x87 FPU Floating-Point Error (Math Fault)");
+			break;
+		case 17:
+			printf("Exception 17 - Alignment Check");
+			break;
+		case 18:
+			printf("Exception 18 - Machine Check");
+			break;
+		case 19:
+			printf("Exception 19 - SIMD Floating-Point Exception");
+			break;
+		case 20:
+			printf("Exception 20 - Virtualization Exception");
+			break;
+		default:
+			break;
+	}
+	halt();
+}
 
+// Exception handler
+void irq_handler(regs_t *regs) {
+	switch (regs->number) {
+		case 0:
+			printf("System timer (PIT)");
+			// timer_handler();
+			break;
+		case 1:
+			printf("Keyboard");
+			// keyboard_handler();
+			break;
+		case 2:
+			printf("Redirected to slave PIC");
+			break;
+		case 3:
+			printf("Serial port (COM2/COM4)");
+			break;
+		case 4:
+			printf("Serial port (COM1/COM3)");
+			break;
+		case 5:
+			printf("Sound card");
+			break;
+		case 6:
+			printf("Floppy disk controller");
+			break;
+		case 7:
+			printf("Parallel port");
+			break;
+		case 8:
+			printf("Real time clock");
+			break;
+		case 9:
+			printf("Redirected to IRQ2");
+			break;
+		case 10:
+			printf("Reserved");
+			break;
+		case 11:
+			printf("Reserved");
+			break;
+		case 12:
+			printf("PS/2 mouse");
+			break;
+		case 13:
+			printf("Math coprocessor");
+			break;
+		case 14:
+			printf("Hard disk controller");
+			break;
+		case 15:
+			printf("Reserved");
+			break;
+		default:
+			break;
+	}
+	pic_eoi(regs->number);
 }
 
 void idt_init() {
+	for (int i = 0; i < IDT_SIZE; i++) {
+		idt[i] = idt_build_entry(0, 0, 0, 0);
+	}
 
+	// CPU exceptions
+	idt[0] = idt_build_entry(GDT_KERNEL_CODE_SELECTOR, (uint32_t) &_exception_0, TYPE_INTERRUPT_GATE, DPL_KERNEL);
+	idt[1] = idt_build_entry(GDT_KERNEL_CODE_SELECTOR, (uint32_t) &_exception_1, TYPE_INTERRUPT_GATE, DPL_KERNEL);
+	idt[2] = idt_build_entry(GDT_KERNEL_CODE_SELECTOR, (uint32_t) &_exception_2, TYPE_INTERRUPT_GATE, DPL_KERNEL);
+	idt[3] = idt_build_entry(GDT_KERNEL_CODE_SELECTOR, (uint32_t) &_exception_3, TYPE_INTERRUPT_GATE, DPL_KERNEL);
+	idt[4] = idt_build_entry(GDT_KERNEL_CODE_SELECTOR, (uint32_t) &_exception_4, TYPE_INTERRUPT_GATE, DPL_KERNEL);
+	idt[5] = idt_build_entry(GDT_KERNEL_CODE_SELECTOR, (uint32_t) &_exception_5, TYPE_INTERRUPT_GATE, DPL_KERNEL);
+	idt[6] = idt_build_entry(GDT_KERNEL_CODE_SELECTOR, (uint32_t) &_exception_6, TYPE_INTERRUPT_GATE, DPL_KERNEL);
+	idt[7] = idt_build_entry(GDT_KERNEL_CODE_SELECTOR, (uint32_t) &_exception_7, TYPE_INTERRUPT_GATE, DPL_KERNEL);
+	idt[8] = idt_build_entry(GDT_KERNEL_CODE_SELECTOR, (uint32_t) &_exception_8, TYPE_INTERRUPT_GATE, DPL_KERNEL);
+	idt[9] = idt_build_entry(GDT_KERNEL_CODE_SELECTOR, (uint32_t) &_exception_9, TYPE_INTERRUPT_GATE, DPL_KERNEL);
+	idt[10] = idt_build_entry(GDT_KERNEL_CODE_SELECTOR, (uint32_t) &_exception_10, TYPE_INTERRUPT_GATE, DPL_KERNEL);
+	idt[11] = idt_build_entry(GDT_KERNEL_CODE_SELECTOR, (uint32_t) &_exception_11, TYPE_INTERRUPT_GATE, DPL_KERNEL);
+	idt[12] = idt_build_entry(GDT_KERNEL_CODE_SELECTOR, (uint32_t) &_exception_12, TYPE_INTERRUPT_GATE, DPL_KERNEL);
+	idt[13] = idt_build_entry(GDT_KERNEL_CODE_SELECTOR, (uint32_t) &_exception_13, TYPE_INTERRUPT_GATE, DPL_KERNEL);
+	idt[14] = idt_build_entry(GDT_KERNEL_CODE_SELECTOR, (uint32_t) &_exception_14, TYPE_INTERRUPT_GATE, DPL_KERNEL);
+	idt[15] = idt_build_entry(GDT_KERNEL_CODE_SELECTOR, (uint32_t) &_exception_15, TYPE_INTERRUPT_GATE, DPL_KERNEL);
+	idt[16] = idt_build_entry(GDT_KERNEL_CODE_SELECTOR, (uint32_t) &_exception_16, TYPE_INTERRUPT_GATE, DPL_KERNEL);
+	idt[17] = idt_build_entry(GDT_KERNEL_CODE_SELECTOR, (uint32_t) &_exception_17, TYPE_INTERRUPT_GATE, DPL_KERNEL);
+	idt[18] = idt_build_entry(GDT_KERNEL_CODE_SELECTOR, (uint32_t) &_exception_18, TYPE_INTERRUPT_GATE, DPL_KERNEL);
+	idt[19] = idt_build_entry(GDT_KERNEL_CODE_SELECTOR, (uint32_t) &_exception_19, TYPE_INTERRUPT_GATE, DPL_KERNEL);
+	idt[20] = idt_build_entry(GDT_KERNEL_CODE_SELECTOR, (uint32_t) &_exception_20, TYPE_INTERRUPT_GATE, DPL_KERNEL);
+
+	// IRQ
+	idt[32] = idt_build_entry(GDT_KERNEL_CODE_SELECTOR, (uint32_t) &_irq_0, TYPE_INTERRUPT_GATE, DPL_KERNEL);
+	idt[33] = idt_build_entry(GDT_KERNEL_CODE_SELECTOR, (uint32_t) &_irq_1, TYPE_INTERRUPT_GATE, DPL_KERNEL);
+	idt[34] = idt_build_entry(GDT_KERNEL_CODE_SELECTOR, (uint32_t) &_irq_2, TYPE_INTERRUPT_GATE, DPL_KERNEL);
+	idt[35] = idt_build_entry(GDT_KERNEL_CODE_SELECTOR, (uint32_t) &_irq_3, TYPE_INTERRUPT_GATE, DPL_KERNEL);
+	idt[36] = idt_build_entry(GDT_KERNEL_CODE_SELECTOR, (uint32_t) &_irq_4, TYPE_INTERRUPT_GATE, DPL_KERNEL);
+	idt[37] = idt_build_entry(GDT_KERNEL_CODE_SELECTOR, (uint32_t) &_irq_5, TYPE_INTERRUPT_GATE, DPL_KERNEL);
+	idt[38] = idt_build_entry(GDT_KERNEL_CODE_SELECTOR, (uint32_t) &_irq_6, TYPE_INTERRUPT_GATE, DPL_KERNEL);
+	idt[39] = idt_build_entry(GDT_KERNEL_CODE_SELECTOR, (uint32_t) &_irq_7, TYPE_INTERRUPT_GATE, DPL_KERNEL);
+	idt[40] = idt_build_entry(GDT_KERNEL_CODE_SELECTOR, (uint32_t) &_irq_8, TYPE_INTERRUPT_GATE, DPL_KERNEL);
+	idt[41] = idt_build_entry(GDT_KERNEL_CODE_SELECTOR, (uint32_t) &_irq_9, TYPE_INTERRUPT_GATE, DPL_KERNEL);
+	idt[42] = idt_build_entry(GDT_KERNEL_CODE_SELECTOR, (uint32_t) &_irq_10, TYPE_INTERRUPT_GATE, DPL_KERNEL);
+	idt[43] = idt_build_entry(GDT_KERNEL_CODE_SELECTOR, (uint32_t) &_irq_11, TYPE_INTERRUPT_GATE, DPL_KERNEL);
+	idt[44] = idt_build_entry(GDT_KERNEL_CODE_SELECTOR, (uint32_t) &_irq_12, TYPE_INTERRUPT_GATE, DPL_KERNEL);
+	idt[45] = idt_build_entry(GDT_KERNEL_CODE_SELECTOR, (uint32_t) &_irq_13, TYPE_INTERRUPT_GATE, DPL_KERNEL);
+	idt[46] = idt_build_entry(GDT_KERNEL_CODE_SELECTOR, (uint32_t) &_irq_14, TYPE_INTERRUPT_GATE, DPL_KERNEL);
+	idt[47] = idt_build_entry(GDT_KERNEL_CODE_SELECTOR, (uint32_t) &_irq_15, TYPE_INTERRUPT_GATE, DPL_KERNEL);
+	
+	idt_ptr.base = (uint32_t) idt; // Base of idt is the first element of idt	
+	idt_ptr.limit = IDT_SIZE - 1; // Limit is the size of IDT - 1
+
+	idt_load(&idt_ptr);
 }
-
--- kernel/idt.h
+++ kernel/idt.h
@@ -1,6 +1,8 @@
 #ifndef _IDT_H_
 #define _IDT_H_
 
+#include "../common/types.h"
+
 // Structure of an IDT descriptor. There are 3 types of descriptors:
 // a task-gate, an interrupt-gate, and a trap-gate.
 // See 5.11 of Intel 64 & IA32 architectures software developer's manual for more details.
@@ -24,5 +26,50 @@ typedef struct idt_ptr_st {
 } __attribute__((packed)) idt_ptr_t;
 
 extern void idt_init();
+extern void idt_load();
+
+// extern void exception_handler(regs_t *regs);
+// extern void irq_handler(regs_t *regs);
+
+// Exception handler
+extern void _exception_0();
+extern void _exception_1();
+extern void _exception_2();
+extern void _exception_3();
+extern void _exception_4();
+extern void _exception_5();
+extern void _exception_6();
+extern void _exception_7();
+extern void _exception_8();
+extern void _exception_9();
+extern void _exception_10();
+extern void _exception_11();
+extern void _exception_12();
+extern void _exception_13();
+extern void _exception_14();
+extern void _exception_15();
+extern void _exception_16();
+extern void _exception_17();
+extern void _exception_18();
+extern void _exception_19();
+extern void _exception_20();
+
+// Interruption handler
+extern void _irq_0();
+extern void _irq_1();
+extern void _irq_2();
+extern void _irq_3();
+extern void _irq_4();
+extern void _irq_5();
+extern void _irq_6();
+extern void _irq_7();
+extern void _irq_8();
+extern void _irq_9();
+extern void _irq_10();
+extern void _irq_11();
+extern void _irq_12();
+extern void _irq_13();
+extern void _irq_14();
+extern void _irq_15();
 
 #endif
